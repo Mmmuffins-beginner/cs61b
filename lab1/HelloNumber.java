bada6d7778d798e9703755465d9b6f037a588b81
Songbing <songbing@sjtu.edu.cn>
2020-09-13 14:10:50 +0800

    completed first part of lab1

---
+++ lab1/HelloNumber.java
@@ -0,0 +1,12 @@
+public class HelloNumber {
+    public static void main(String[] args) {
+        int x;
+        int y=0;
+        x=0;
+        while(x<10){
+            System.out.print(y+" ");
+            x=x+1;
+            y=y+x;
+        }
+    }
+}
